2b58ad1bb3c2636d3474f352937c04ff0a6511f0
makushov <stanislav.makushov@gmail.com>
2021-10-08 13:48:17 +0300

    Initial Commit

---
+++ RoboSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RoboSwift
+//
+//  Created by Stanislav Makushov on 08.10.2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
